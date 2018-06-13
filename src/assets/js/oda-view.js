4194463960dace016b3d8e33176ccb509c95a4a9
Kinich Barcelo <kinich.barcelo@code-ing.com>
2018-06-13 16:58:36 -0500

    QOL life imporovements to momento and odas

--- src/assets/js/oda-view.js
+++ src/assets/js/oda-view.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function () {
     $('#fullpage').fullpage({
-      anchors: ['firstPage', 'secondPage', '3rdPage'],
+      anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
       // sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
       navigation: true,
       navigationPosition: 'left',
       // navigationTooltips: ['First page', 'Second page', 'Third and last page'],
-      paddingBottom: '5rem',
+      paddingBottom: '3rem',
+      paddingTop: '3rem',
+      verticalCentered: false,
     });
   });
   
